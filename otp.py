e77862310faba1368b8a68b10f248995c23dae52
Gavin Hills <ghills@gmail.com>
2013-11-09 12:22:34 -0800

    Simplify implementation a little bit and add HTOP.

--- otp.py
+++ otp.py
@@ -1,48 +1,45 @@
-import base64
-import time
 import hashlib
 import hmac
 import struct
 
-def LocalToNetworkByteArray( val ):
-    ret = bytearray(8)
-    ret[0] = 0
-    ret[1] = 0
-    ret[2] = 0
-    ret[3] = 0
-    ret[4] = ( val & 0xFF000000 ) >> 24
-    ret[5] = ( val & 0x00FF0000 ) >> 16
-    ret[6] = ( val & 0x0000FF00 ) >> 8
-    ret[7] = ( val & 0x000000FF )
-    return ret
+def int_to_big_endian_bytes( val ):
+    # converts a value to a big endian ordered 8 byte array
+    # used for hash input
+    return bytearray( struct.pack('>Q',val) )
 
-def GetSHA1Digest( key, counter ):
+def sha1_digest( key, counter ):
     return bytearray(hmac.new( key=key, msg=counter, digestmod=hashlib.sha1 ).digest())
 
-def GetBinCode( digest ):
-    offset = digest[19] & 0xF
-    bin_code = ( digest[offset] & 0x7F ) << 24 \
-        | ( digest[offset+1] & 0xFF ) << 16    \
-        | ( digest[offset+2] & 0xFF ) << 8     \
-        | ( digest[offset+3] & 0xFF )
+def dynamic_truncation( hmac_result ):
+    # HMAC SHA-1 algorithm should output 20 bytes
+    assert( len(hmac_result) == 20 )
+
+    offset = hmac_result[19] & 0xF
+    bin_code = ( hmac_result[offset] & 0x7F ) << 24     \
+             | ( hmac_result[offset+1] & 0xFF ) << 16   \
+             | ( hmac_result[offset+2] & 0xFF ) << 8    \
+             | ( hmac_result[offset+3] & 0xFF )
     return bin_code
 
-def ScaleToNDigits( val, n ):
-    return( val % ( 10**n ) )
+def hotp_code( secret, counter, digits=6 ):
+    if digits < 6 or digits > 8:
+        # the RFC only allows 6-8 digits
+        raise Exception('Invalid number of digits: {}'.format(digits))
 
-def GetTimeCount( time_interval ):
-    return int(time.time() / time_interval)
+    count_hash = int_to_big_endian_bytes(counter)
+    digest = sha1_digest(key=secret,counter=count_hash)
+    bin_code = dynamic_truncation(digest)
+    return ( bin_code % ( 10**digits ) )
 
-def GetCurrentTOTPCode(secret, interval=30 ):
-    count = GetTimeCount( interval )
-    count_hash = LocalToNetworkByteArray(count)
-    digest = GetSHA1Digest(key=secret,counter=count_hash)
-    bin_code = GetBinCode(digest)
-    return ScaleToNDigits( bin_code, 6 )
+def totp_code( secret, interval=30, digits=6 ):
+    import time
+
+    count = int(time.time() / interval )
+    return hotp_code( secret, count, digits )
 
 def main():
     import fileinput
-
+    import base64
     for line in fileinput.input():
         line = line.strip().upper()
         try:
@@ -50,7 +47,7 @@ def main():
         except TypeError:
             print 'Unable to decode {} as a based 32 string.'.format(line)
             return -1
-        print '{0:06d}'.format(GetCurrentTOTPCode(secret))
+        print '{0:06d}'.format(totp_code(secret))
 
 if __name__ == '__main__':
     main()
